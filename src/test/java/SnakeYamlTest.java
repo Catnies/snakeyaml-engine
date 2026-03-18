0f3a82455a3985b33a872a3522613e46ec0237c0
Catnies <catnies@foxmail.com>
2026-03-18 22:38:19 +0800

    测试类

---
+++ src/test/java/SnakeYamlTest.java
@@ -0,0 +1,38 @@
+package org.snakeyaml.engine;
+
+import org.snakeyaml.engine.v2.api.*;
+import org.snakeyaml.engine.v2.api.lowlevel.Compose;
+import org.snakeyaml.engine.v2.nodes.MappingNode;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class SnakeYamlTest {
+    public static void main(String[] args) throws IOException {
+
+        InputStream resource =
+                SnakeYamlTest
+                        .class
+                        .getClassLoader()
+                        .getResourceAsStream("input2.yml");
+
+        LoadSettings settings = LoadSettings.builder().setParseComments(true).build();
+        Compose compose = new Compose(settings);
+        MappingNode mappingNode = (MappingNode) compose.composeInputStream(resource).get();
+
+        DumpSettings dumpSettings = DumpSettings.builder().setIndicatorIndent(2)
+                .setIndentWithIndicator(true).setDumpComments(true).build();
+        Dump dump = new Dump(dumpSettings);
+
+        Path outputPath = Path.of("output.yml");
+        OutputStream outputStream = Files.newOutputStream(outputPath);
+        YamlOutputStreamWriter yamlOutputStreamWriter =
+                new YamlOutputStreamWriter(outputStream, StandardCharsets.UTF_8);
+
+        dump.dumpNode(mappingNode, yamlOutputStreamWriter);
+    }
+}
